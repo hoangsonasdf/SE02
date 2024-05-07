42c2edd8d761d8bfcaa0117cf499edc999caa9a0
hoangsonasdf <108865470+hoangsonasdf@users.noreply.github.com>
2024-05-07 22:43:20 +0700

    Initial commit

---
+++ src/main/java/fit/se02/team7/chic_lighting/ChicLightingApplication.java
@@ -0,0 +1,13 @@
+package fit.se02.team7.chic_lighting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicLightingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicLightingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fit/se02/team7/chic_lighting/ChicLightingApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.se02.team7.chic_lighting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicLightingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
